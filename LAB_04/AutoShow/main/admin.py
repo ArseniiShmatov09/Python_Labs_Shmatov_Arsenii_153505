8e9dcff6b943bf90c9b66610eef2f4ec3157f80c
ArseniiShmatov09 <shmatovarsenii09@gmail.com>
2023-05-28 21:15:00 +0300

    added models, start working with templates

--- LAB_04/AutoShow/main/admin.py
+++ LAB_04/AutoShow/main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Car, CarcassType, Producer, Client
 
 # Register your models here.
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'model', 'year_of_publication', 'description', 'cost', 'color', 'carcass', 'producer'] 
+    list_filter = ['brand', 'year_of_publication', 'cost', 'carcass']
+
+@admin.register(CarcassType)
+class CarcassTypeAdmin(admin.ModelAdmin):
+    list_display = ['designation']
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+    list_display = ['country', 'owner']
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_of_birth','email', 'phone_number' ]
+    list_filter = ['first_name', 'last_name', 'date_of_birth','email', 'phone_number' ]
+
+
+
+
+
+
--- LAB_04/AutoShow/main/views.py
+++ LAB_04/AutoShow/main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h2>hui</h2>")
+ cars  = Car.objects.all()[:5]
+ return render(request, 'index.html', context={'cars':cars})
+
+def CarsList(request):
+ cars  = Car.objects.all()
+ return render(request, 'cars.html', context={'cars':cars})
